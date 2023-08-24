45478eb37c0f6cb73185b958367265afcba2967f
Munir Zaman <munir.zaman2k22@outlook.com>
2023-08-24 14:26:36 +0600

    Formatted code

--- Graph Algorithms/Building Roads.cpp
+++ Graph Algorithms/Building Roads.cpp
@@ -1,26 +1,34 @@
 #include "bits/stdc++.h"
 
 template <typename T>
-void print_vec(std::vector<T> vec) {
-    for (const auto& x : vec) std::cout << x << " ";
+void print_vec(std::vector<T> vec)
+{
+    for (const auto &x : vec)
+        std::cout << x << " ";
     std::cout << "\n";
 }
 
-void dfs(int a, std::vector<std::vector<int>> &adj, std::vector<bool> &visited) {
-    visited[a-1] = true;
-    for (const auto& neighbor: adj[a-1]) {
-        if (!visited[neighbor-1]) {
+void dfs(int a, std::vector<std::vector<int>> &adj, std::vector<bool> &visited)
+{
+    visited[a - 1] = true;
+    for (const auto &neighbor : adj[a - 1])
+    {
+        if (!visited[neighbor - 1])
+        {
             dfs(neighbor, adj, visited);
         }
     }
 }
 
-int count_components(std::vector<std::vector<int>> &adj, int n) {
+int count_components(std::vector<std::vector<int>> &adj, int n)
+{
     std::vector<bool> visited(n, false);
 
     int components = 0;
-    for (int node = 1; node <= n; node++) {
-        if (!visited[node - 1]) {
+    for (int node = 1; node <= n; node++)
+    {
+        if (!visited[node - 1])
+        {
             components++;
             dfs(node, adj, visited);
         }
@@ -29,14 +37,17 @@ int count_components(std::vector<std::vector<int>> &adj, int n) {
     return components;
 }
 
-void solve(std::vector<std::vector<int>> &adj, int n) {
+void solve(std::vector<std::vector<int>> &adj, int n)
+{
     std::vector<bool> visited(n, false);
 
     std::vector<int> roads;
 
     int components = 0;
-    for (int node = 1; node <= n; node++) {
-        if (!visited[node - 1]) {
+    for (int node = 1; node <= n; node++)
+    {
+        if (!visited[node - 1])
+        {
             roads.push_back(node);
             components++;
             dfs(node, adj, visited);
@@ -44,23 +55,25 @@ void solve(std::vector<std::vector<int>> &adj, int n) {
     }
 
     std::cout << components - 1 << "\n";
-    for (int i = 1; i < components; i++) {
+    for (int i = 1; i < components; i++)
+    {
         std::cout << roads[0] << " " << roads[i] << "\n";
     }
-
 }
 
-int main() {
+int main()
+{
     int n, m; // cities, roads
     std::cin >> n >> m;
 
     std::vector<std::vector<int>> adj(n);
 
-    while (m--) {
+    while (m--)
+    {
         int a, b;
         std::cin >> a >> b;
-        adj[a-1].push_back(b);
-        adj[b-1].push_back(a);
+        adj[a - 1].push_back(b);
+        adj[b - 1].push_back(a);
     }
 
     solve(adj, n);
--- Graph Algorithms/Message Route.cpp
+++ Graph Algorithms/Message Route.cpp
@@ -1,13 +1,15 @@
 #include "bits/stdc++.h"
 
 template <typename T>
-void print_vec(std::vector<T> vec) {
-    for (const auto& x : vec) std::cout << x << " ";
+void print_vec(std::vector<T> vec)
+{
+    for (const auto &x : vec)
+        std::cout << x << " ";
     std::cout << "\n";
 }
 
-
-void solve(int n, std::vector<std::vector<int>> &adj) {
+void solve(int n, std::vector<std::vector<int>> &adj)
+{
     std::vector<bool> visited(n, false);
     std::vector<int> prev(n, -1);
 
@@ -16,17 +18,20 @@ void solve(int n, std::vector<std::vector<int>> &adj) {
     processing.push(1);
     visited[0] = true;
 
-    while (!processing.empty()) {
+    while (!processing.empty())
+    {
         // while not finished processing
         // get the current unprocessed node
         int node = processing.front();
 
         // process the neighbors
-        for (const auto &neighbor : adj[node-1]) {
-            if (!visited[neighbor-1]) {
-                visited[neighbor-1] = true;
+        for (const auto &neighbor : adj[node - 1])
+        {
+            if (!visited[neighbor - 1])
+            {
+                visited[neighbor - 1] = true;
                 processing.push(neighbor);
-                prev[neighbor-1] = node;
+                prev[neighbor - 1] = node;
             }
         }
 
@@ -38,32 +43,38 @@ void solve(int n, std::vector<std::vector<int>> &adj) {
     int current_node = n;
     std::vector<int> path;
 
-    while (current_node != -1) {
+    while (current_node != -1)
+    {
         path.push_back(current_node);
-        current_node = prev[current_node-1];
+        current_node = prev[current_node - 1];
     }
 
     std::reverse(path.begin(), path.end());
-    if (path[0] == 1) {
+    if (path[0] == 1)
+    {
         std::cout << path.size() << "\n";
         print_vec(path);
-    } else {
-        std::cout << "IMPOSSIBLE" << "\n";
+    }
+    else
+    {
+        std::cout << "IMPOSSIBLE"
+                  << "\n";
     }
 }
 
-
-int main() {
+int main()
+{
     int n, m;
     std::cin >> n >> m;
 
     std::vector<std::vector<int>> adj(n);
 
-    while (m--) {
+    while (m--)
+    {
         int a, b;
         std::cin >> a >> b;
-        adj[a-1].push_back(b);
-        adj[b-1].push_back(a);
+        adj[a - 1].push_back(b);
+        adj[b - 1].push_back(a);
     }
 
     solve(n, adj);
