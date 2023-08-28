8db87325a8e8918b8da7abe3d3783739ae43308b
Munir Zaman <munir.zaman2k22@outlook.com>
2023-08-28 14:26:05 +0600

    updated Subordinates.cpp

--- Tree Algorithms/Subordinates.cpp
+++ Tree Algorithms/Subordinates.cpp
@@ -1,78 +1,48 @@
 #include "bits/stdc++.h"
 
-// NOTE: Getting TLE on some test cases
+// NOTE: have to properly rewrite the solution (it works btw! :D)
 
-struct Node {
-    int value;
-    // keep track of number of descendants
-    int descendants = 0;
-    Node* parent;
-    std::vector<Node*> children;
-
-    void increase_descendants(int n) {
-        descendants += n;
-        if (parent != nullptr) {
-            // if has parent, increase descendants of parent
-            parent->increase_descendants(n);
-        }
+void update(int n, std::map<int, std::vector<int>> &children, std::vector<int> &descendants) {
+    descendants[n-1] = 0; // has been processed
+    for (auto &child : children[n]) {
+        // update descendants for children not yet processed
+        if (descendants[child-1] == -1) { update(child, children, descendants); }
     }
-
-    void add_child(Node *child) {
-        child->parent = this;
-        this->children.push_back(child);
-
-        this->increase_descendants(1 + child->descendants);
-    }
-
-    void make_parent(Node *p) {
-        p->add_child(this);
-    }
-
-};
-
-void print_children(Node n) {
-    std::cout << n.value << " ( " << n.descendants << " ) " << " -> ";
-    for (auto x : n.children) {
-        std::cout << x->value << " ";
-    }
-    std::cout << "\n";
-}
-
-void print_tree(Node* n) {
-    if (n->children.empty()) {
-        std::cout << n->value << " -> null" << "\n";
-    } else {
-        print_children(*n);
-        for (auto x : n->children) {
-            print_tree(x);
-        }
+    for (auto &child : children[n]) {
+        // update descendants for current node n
+        descendants[n-1] += descendants[child-1] + 1;
     }
 }
 
 
 int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-
     int n;
     std::cin >> n;
 
-    std::vector<Node> employees(n);
-    employees[0].value = 1;
-    for (int i = 1; i < n; i++) {
-        // set value of employee
-        employees[i].value = i+1;
-
-        // input parent value for i+1 th employee
-        int parent;
-        std::cin >> parent;
-
-        // set parent
-        employees[i].make_parent(&employees[parent-1]);
-    }
+    // <child, parent> map
+    std::map<int, int> parents;
+    for (int i = 1; i <= n; i++) { parents[i] = 0; }
     
-    for (auto &e : employees) {
-        std::cout << e.descendants << " ";
+    // <parent, children> map
+    std::map<int, std::vector<int>> children;
+    for (int i = 1; i <= n; i++) { children[i] = {}; }
+
+    for (int i = 1; i < n; i++) {
+        int p; // parent of i+1
+        std::cin >> p;
+
+        parents[i+1] = p;
+        children[p].push_back(i+1);
+
+    }
+
+    std::vector<int> descendants(n, -1);
+    // update descendants
+    update(1, children, descendants);
+    
+    // print out descendants
+    for (auto &x : descendants) {
+        std::cout << x << " ";
     }
     std::cout << "\n";
 }
