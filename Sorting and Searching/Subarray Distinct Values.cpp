7a00be7516f72543a5bbd51f1f37137d625f6042
Munir Zaman <munir.zaman2k22@outlook.com>
2024-01-01 15:05:03 +0600

    line breaks :/

---
+++ Sorting and Searching/Subarray Distinct Values.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+
+int main() {
+    ll n, k;
+    std::cin >> n >> k;
+
+    std::vector<ll> nums(n,0);
+    for(auto &v : nums) std::cin >> v;
+    
+}
