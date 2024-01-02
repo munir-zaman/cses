0bdef9ea0277f66bfdb1f6361218feeb2eec3ac5
Munir Zaman <munir.zaman2k22@outlook.com>
2024-01-02 22:04:54 +0600

    tried :/

--- Sorting and Searching/Subarray Distinct Values.cpp
+++ Sorting and Searching/Subarray Distinct Values.cpp
@@ -1,42 +1,36 @@
 #include<bits/stdc++.h>
 typedef long long ll;
 
-int count_distincts(std::map<ll,int> N) {
-    int ans = 0;
-    for (auto &x : N) {
-        if (x.second > 0) ans++;
-    }
-    return ans;
-}
-
-
 int main() {
     ll n, k;
     std::cin >> n >> k;
 
     std::vector<ll> nums(n,0);
     for(auto &v : nums) std::cin >> v;
-    
-    std::vector<ll> repeats;
-    ll last_num = nums[0];
-    ll counter = 0;
-    for (int i = 0; i<n; i++) {
-        if (last_num == nums[i]) counter++;
-        else {
-            repeats.push_back(counter);
-            counter = 1;
-        }
-        last_num = nums[i];
-    }
 
-    repeats.push_back(counter);
+    ll counter = 0;
+    ll last_num = nums[0];
+    std::queue<ll> repeat_start;
+    std::map<ll, bool> seen;
 
     ll ans = 0;
-    ll sum = 0;
-    for (int i = 0; i < repeats.size(); i++) {
-        if (i-k>=0) sum = sum - repeats[i-k];
-        ans += (1 << repeats[i]) - 1 + repeats[i]*sum;
-        sum = sum + repeats[i];
+    for (int i = 0; i<n; i++) {
+        if (last_num != nums[i]) {
+            if (!seen[nums[i]]) {
+                // increase counter if encountered a new value not seen before
+                counter++;
+                seen[nums[i]] = true;
+            }
+            repeat_start.push(nums[i]);
+        }
+
+        if (counter>k) {
+            counter--;
+            seen[nums[i]] = false;
+        }
+        
+        ans += i - repeat_start.front() + 1;
+        // update last_num
+        last_num = nums[i];
     }
-    std::cout << ans << "\n";
 }
