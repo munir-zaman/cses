9d69643aec73ceb7a2ae2b7a8b0799e2de807bb2
Munir Zaman <munir.zaman2k22@outlook.com>
2024-10-01 14:10:43 +0600

    Palindrome Reorder

--- Introductory Problems/Palindrome Reorder.cpp
+++ Introductory Problems/Palindrome Reorder.cpp
@@ -2,35 +2,39 @@
 #define ll long long
 
 int main() {
-
     std::string s;
     std::cin >> s;
-
-    std::vector<int> freq(26,0);
+    int n = s.size();
+    
+    // frequency array
+    std::vector<int> freq(26, 0);
     for (const char &x: s) {
-        freq[(int)(x-'A')]++;
+        freq[x-'A']++;
     }
 
-    std::string p;
+    int i = 0;
+    int odd_index = -1;
     int odd_count = 0;
-    int odd_char = 0;
-    for (int c=0; c<26; c++) {
-        if (freq[c]%2==0) {
-            for (int k=0; k<freq[c]/2; k++) {
-                p.push_back((char)(c+'A'));
-            }
-        }
-        else {
+    for (int j=0; j<26; j++) {
+        // count odd frequencies
+        if (freq[j]%2 != 0) {
             odd_count++;
-            odd_char = c;   
+            odd_index = j;
+        }
+
+        if (odd_count > 1) break;
+
+        while (freq[j] > 1) {
+            s[i] = s[n-i-1] = j + 'A';
+            i++;
+            freq[j] -= 2;
         }
     }
 
-    p.push_back((char)(odd_char+'A'));
-    if (odd_count > 1) std::cout << "NO SOLUTION\n";
-    else {
-        std::string temp_p = p;
-        for (int i=temp_p.size()-1-odd_count; i>=0; i--) p.push_back(temp_p[i]);
-        std::cout << p << "\n";
+    if (odd_count > 1) {
+        printf("NO SOLUTION\n");
+    } else {
+        if (odd_count == 1) s[i] = odd_index + 'A';
+        std::cout << s << std::endl;
     }
 }
